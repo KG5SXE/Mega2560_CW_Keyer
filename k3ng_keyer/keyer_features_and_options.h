a5e5956f44fb8eb4c1f94b894b7c03da15932d69
zedosan <rmagdaleno@gmail.com>
2017-05-01 18:20:43 -0500

    Added FEATURE Sunfounder I2C LCD

--- k3ng_keyer/keyer_features_and_options.h
+++ k3ng_keyer/keyer_features_and_options.h
@@ -26,6 +26,7 @@
 // #define FEATURE_LCD_YDv1                // YourDuino I2C LCD display with old LCM 1602 V1 ic
 // #define FEATURE_LCD1602_N07DH      // http://linksprite.com/wiki/index.php5?title=16_X_2_LCD_Keypad_Shield_for_Arduino
 // #define FEATURE_LCD_SAINSMART_I2C
+#define FEATURE_LCD_SUNFOUNDER_I2C_KG5SXE	//Sunfounder I2C 20x4 LCD display using LCM1602 by KG5SXE
 // #define FEATURE_CW_DECODER
 // #define FEATURE_SLEEP                   // go to sleep after x minutes to conserve battery power (not compatible with Arduino DUE, may have mixed results with Mega and Mega ADK)
 // #define FEATURE_ROTARY_ENCODER          // rotary encoder speed control
